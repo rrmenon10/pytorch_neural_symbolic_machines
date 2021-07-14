a3c029e4e904e6d66829f0fa84b1add2e945cdc1
rrmenon10 <rockmenon@icloud.com>
2021-07-14 21:55:25 +0530

    adD

--- bin/init.sh
+++ bin/init.sh
@@ -1,8 +1,10 @@
-bash bin/setup_env.sh
-/opt/conda/bin/conda init
+/opt/conda/bin/conda init bash
 source ~/.bashrc
-
-## Install the nsm environment
+git clone https://github.com/facebookresearch/TaBERT.git
+cd TaBERT
+bash scripts/setup_env.sh
+conda activate tabert
+pip install --editable .
 conda env update --name tabert --file data/env.yml
 conda deactivate
 
