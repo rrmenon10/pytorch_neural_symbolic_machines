6204d03be82afcbbaece3303666db6dfcff06fff
rrmenon10 <rockmenon@icloud.com>
2021-07-14 21:02:41 +0530

    add

--- bin/train.sh
+++ bin/train.sh
@@ -1,11 +1,10 @@
 
 bert_size=$1
 
-conda activate tabert
-echo "OMP_NUM_THREADS=1 python -m \
+OMP_NUM_THREADS=1 /opt/conda/envs/tabert/bin/python -m \
   table.experiments \
   train \
   seed 0 \
   --cuda \
   --work-dir=runs/bert_${bert_size} \
-  --config=data/config/config.vanilla_bert_${bert_size}.json"
+  --config=data/config/config.vanilla_bert_${bert_size}.json
