db9b8c332ae8288aed7761f26480589994eaaaff
rrmenon10 <rockmenon@icloud.com>
2021-07-14 20:00:33 +0530

    tiny

--- bin/write_output.sh
+++ bin/write_output.sh
@@ -1,2 +1,4 @@
 bert_config=$1
 gsutil -m -o GSUtil:parallel_composite_upload_threshold=150M cp -r runs/bert_${bert_config} gs://inverse-semantics/bert_${bert_config}
+gsutil -m -o GSUtil:parallel_composite_upload_threshold=150M cp -r output gs://inverse-semantics/${bert_config}
+
